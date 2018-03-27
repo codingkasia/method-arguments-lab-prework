6e7ccff83a6a9587ae2519cc0f3c77f92dcaf11a
KATARZYNA ROSENBERG <codingkasia@gmail.com>
2018-03-27 21:12:28 +0000

    automatically backed up by learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,5 +1,5 @@
 def introduction(name) 
-  puts "Hi, my nameis.\n" + name'
+  puts "Hi, my nameis.\n" + name;
 end
 def introduction_with_language(name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
